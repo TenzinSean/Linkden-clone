bb3d2cdb46d97a3402334bfa3e5416119441c468
Tenzin Sean <tenzin.jeunesse@gmail.com>
2021-01-05 17:11:24 +0100

    added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Lets build the linkden clone</h1> */}
+
+      {/* Header */}
+      <Header />
+          
+                {/* App Body */}
+
+          {/* Sidebar */}
+
+      {/* Feed */}
+
+          {/* Widgets */}
+
+      
+    </div>
+  );
+}
+
+export default App;
